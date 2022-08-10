313ecb00217f170215e499ed83978f8afc1dd258
yukisato <yuki01377@gmail.com>
2022-08-11 01:51:00 +0900

    add user edit and delete

--- BookBooking/Controllers/AccountController.cs
+++ BookBooking/Controllers/AccountController.cs
@@ -10,115 +10,186 @@ using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
 
 namespace BookBooking.Controllers
+{
+    public class AccountController : ControllerBase
     {
-        public class AccountController : ControllerBase
+        private readonly BookContext _context;
+        private readonly ILogger<BooksController> _logger;
+
+        public AccountController(
+            ILogger<BooksController> logger,
+            BookContext context
+            )
         {
-            private readonly BookContext _context;
-            private readonly ILogger<BooksController> _logger;
+            _logger = logger;
+            _context = context;
+        }
 
-            public AccountController(
-                ILogger<BooksController> logger,
-                BookContext context
-                )
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var email = User.Claims.FirstOrDefault(x => x.Type.ToLower().Contains("emailaddress")).Value;
+            var currentUser = _context.Users.FirstOrDefault(x => x.Email == email);
+
+            var viewModel = new UserInfoViewModel
             {
-                _logger = logger;
-                _context = context;
+                Id = currentUser.Id,
+                Name = currentUser.Name,
+                Email = currentUser.Email,
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ActionName(nameof(Login))]
+        public async Task<IActionResult> PostLogin(LoginViewModel viewModel)
+        {
+            var users = _context.Users.ToList();
+            var currentUser = users.FirstOrDefault(x => x.Email == viewModel.Email);
+            if (currentUser == null)
+            {
+                ModelState.AddModelError("Email", "メールアドレスが登録されていません。");
+                return View(viewModel);
             }
-
-            [HttpGet]
-            public IActionResult Login(string returnUrl)
+            var isVerfyPassword = new PasswordHasher<ClaimsPrincipal>().VerifyHashedPassword(User, currentUser.Password, viewModel.Password) == PasswordVerificationResult.Success;
+            //else if (currentUser.Password != viewModel.Password)
+            if (!isVerfyPassword)
             {
-                return View();
-            }
-
-            [HttpGet]
-            public IActionResult Register()
-            {
-                return View();
-            }
-
-            [HttpPost]
-            [ActionName(nameof(Login))]
-            public async Task<IActionResult> PostLogin(LoginViewModel viewModel)
-            {
-                var users = _context.Users.ToList();
-                var currentUser = users.FirstOrDefault(x => x.Email == viewModel.Email);
-                if (currentUser == null)
-                {
-                    ModelState.AddModelError("Email", "メールアドレスが登録されていません。");
-                    return View(viewModel);
-                }
-                var isVerfyPassword = new PasswordHasher<ClaimsPrincipal>().VerifyHashedPassword(User, currentUser.Password, viewModel.Password) == PasswordVerificationResult.Success;
-                //else if (currentUser.Password != viewModel.Password)
-                if (!isVerfyPassword)
-                {
-                    ModelState.AddModelError("Password", "パスワードが一致しません。");
-                    return View(viewModel);
-                }
-
-                var claims = new Claim[]
-                {
-                    new Claim(ClaimTypes.PrimarySid, currentUser.Id.ToString()),
-                    new Claim(ClaimTypes.Email, currentUser.Email),
-                    new Claim(ClaimTypes.Role, currentUser.Role.ToString())
-                };
-
-                var identity = new ClaimsIdentity(claims,
-                    CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var principal = new ClaimsPrincipal(identity);
-
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                    principal);
-                SetFlash(FlashMessageType.Success, "ログインに成功しました。");
-                return RedirectToAction("Index", "Books");
-            }
-
-            [HttpPost]
-            [ActionName(nameof(Register))]
-            public IActionResult PostRegister(RegisterViewModel viewModel)
-            {
-                // validation
-                if (!ModelState.IsValid)
-                {
-                    return View(viewModel);
-                }
-                var model = new User() {
-                    Name = viewModel.Username,
-                    Email = viewModel.Email,
-                    Password = new PasswordHasher<ClaimsPrincipal>().HashPassword(User, viewModel.Password),
-                    Role = viewModel.Role
-                };
-
-                _context.Add(model);
-                try
-                {
-                    _context.SaveChanges();
-                    SetFlash(FlashMessageType.Success, "登録に成功しました。");
-                    return RedirectToAction(nameof(Login));
-                }
-                catch (DbUpdateException ex)
-                when (ex.InnerException is MySqlException)
-                {
-                    if (ex.InnerException.Message.Contains("Duplicate"))
-                    {
-                        ModelState.AddModelError("Email", "メールアドレスが既に登録されています。");
-                    }
-                }
+                ModelState.AddModelError("Password", "パスワードが一致しません。");
                 return View(viewModel);
             }
 
-            [Authorize]
-            public async Task<IActionResult> Logout()
+            var claims = new Claim[]
             {
-                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-                return RedirectToAction();
+                new Claim(ClaimTypes.PrimarySid, currentUser.Id.ToString()),
+                new Claim(ClaimTypes.Email, currentUser.Email),
+                new Claim(ClaimTypes.Role, currentUser.Role.ToString())
+            };
+
+            var identity = new ClaimsIdentity(claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                principal);
+            SetFlash(FlashMessageType.Success, "ログインに成功しました。");
+            return RedirectToAction("Index", "Books");
+        }
+
+        [HttpPost]
+        [ActionName(nameof(Register))]
+        public IActionResult PostRegister(RegisterViewModel viewModel)
+        {
+            // validation
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            var model = new User() {
+                Name = viewModel.Username,
+                Email = viewModel.Email,
+                Password = new PasswordHasher<ClaimsPrincipal>().HashPassword(User, viewModel.Password),
+                Role = viewModel.Role
+            };
+
+            _context.Add(model);
+            try
+            {
+                _context.SaveChanges();
+                SetFlash(FlashMessageType.Success, "登録に成功しました。");
+                return RedirectToAction(nameof(Login));
+            }
+            catch (DbUpdateException ex)
+            when (ex.InnerException is MySqlException)
+            {
+                if (ex.InnerException.Message.Contains("Duplicate"))
+                {
+                    ModelState.AddModelError("Email", "メールアドレスが既に登録されています。");
+                }
+            }
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(UserInfoViewModel viewModel)
+        {
+            // validation
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
             }
 
-            public class LoginRequest
+            var currentUser = _context.Users.FirstOrDefault(x => x.Id == viewModel.Id);
+
+            var isVerfyPassword = new PasswordHasher<ClaimsPrincipal>().VerifyHashedPassword(User, currentUser.Password, viewModel.Password) == PasswordVerificationResult.Success;
+            if (!isVerfyPassword)
             {
-                public string username { get; set; }
-                public string email { get; set; }
+                ModelState.AddModelError("Password", "パスワードが一致しません。");
+                return View(viewModel);
             }
+
+            if (!string.IsNullOrEmpty(viewModel.NewPassword)) currentUser.Password = new PasswordHasher<ClaimsPrincipal>().HashPassword(User, viewModel.NewPassword);
+            if (!string.IsNullOrEmpty(viewModel.Name)) currentUser.Name = viewModel.Name;
+            if (!string.IsNullOrEmpty(viewModel.Email)) currentUser.Email = viewModel.Email;
+
+            try
+            {
+                _context.Update(currentUser);
+                _context.SaveChanges();
+                SetFlash(FlashMessageType.Success, "更新に成功しました。");
+                return RedirectToAction("Index", "Books");
+            }
+            catch (DbUpdateException ex)
+            when (ex.InnerException is MySqlException)
+            {
+                if (ex.InnerException.Message.Contains("Duplicate"))
+                {
+                    ModelState.AddModelError("Email", "メールアドレスが既に登録されています。");
+                }
+            }
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Delete(UserInfoViewModel viewModel)
+        {
+            var entity = _context.Users.FirstOrDefault(x => x.Id == viewModel.Id);
+            _context.Users.Remove(entity);
+            _context.SaveChanges();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            SetFlash(FlashMessageType.Success, "ユーザーの削除に成功しました。");
+            //return RedirectToAction(nameof(Logout));
+            return RedirectToAction();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            SetFlash(FlashMessageType.Success, "ログアウトしました。");
+            return RedirectToAction();
+        }
+
+        public class LoginRequest
+        {
+            public string username { get; set; }
+            public string email { get; set; }
         }
     }
+}
---
+++ BookBooking/Models/UserInfoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookBooking.Models
+{
+    public class UserInfoViewModel
+    {
+        [Required]
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "名前")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "メールアドレス")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "新しいパスワード(変更したくない場合は空欄のままにしてください)")]
+        public string? NewPassword { get; set; }
+    }
+}
+
